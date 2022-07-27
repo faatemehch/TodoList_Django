39d3fa86858902808b814bac4c0e49b9b3c79d67
faatemehch <fch199674@yahoo.com>
2022-07-27 11:17:16 +0430

    bug fixes

--- TodoList/settings.py
+++ TodoList/settings.py
@@ -36,8 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    # 3rd party app
     'mathfilters',
+    # local app
     'task',
     'forgot_password'
 ]
